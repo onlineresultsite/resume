cc8e8715c52adace56da2e62f8dcf18aae3b8a91
onlineresultsite <footflux@gmail.com>
2024-07-13 12:26:34 +0530

    name change

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash 
 
 
-sed -i 's/\[]/\["13.51.109.155"]/' /home/ubuntu/Resume/settings.py
+
 cd /home/ubuntu/Resume
 python3 manage.py migrate 
 python3 manage.py makemigrations     
 python3 manage.py collectstatic --noinput
 sudo service gunicorn restart
 sudo service nginx restart
+cd /home/ubuntu/Resume
 #sudo tail -f /var/log/nginx/error.log
 #sudo systemctl reload nginx
 #sudo tail -f /var/log/nginx/error.log
